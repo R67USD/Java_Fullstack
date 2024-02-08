7e81a8f24f016affae0df47ea50182155f9150d4
Rohan <rohanpadmukh21@gmail.com>
2024-02-08 10:59:59 +0530

    08_02_24

---
+++ JS/08_02_24/myscript.js
@@ -0,0 +1,17 @@
+// Declare a variable `empname` to store employee name and assign it the string "Sonal"
+var empname = "Sonal";
+
+// Declare a variable `id` to store employee ID and assign it the number 1001
+var id = 1001;
+
+// Declare a variable `sal` to store employee salary and assign it the floating-point number 56000.0
+var sal = 56000.0;
+
+// Write the employee ID with a label and a line break using `document.write`
+document.write("Id is " + id + "<br>");
+
+// Write the employee name with a label and a line break using `document.write`
+document.write("Name is " + empname + "<br>");
+
+// Write the employee salary with a label using `document.write`
+document.write("Salary is " + sal);
